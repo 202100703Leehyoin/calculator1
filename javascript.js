f94f35ad0e24bb8079e94533441c86740497460a
202100703Leehyoin <130342354+202100703Leehyoin@users.noreply.github.com>
2023-05-01 23:39:33 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,31 +1,22 @@
-let output = document.querySelector(".output");
-let history = document.querySelector(".history");
-let buttons = document.querySelectorAll(".button");
+let currentDisplay = "0";
 
-for (let i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener("click", function() {
-		let buttonValue = this.innerHTML;
-		if (buttonValue == "AC") {
-			output.innerHTML = "0";
-			history.innerHTML = "";
-		}
-		else if (buttonValue == "+/-") {
-			output.innerHTML = eval("-1 * " + output.innerHTML);
-		}
-		else if (buttonValue == "%") {
-			output.innerHTML = eval(output.innerHTML + "/100");
-		}
-		else if (buttonValue == "=") {
-			history.innerHTML = output.innerHTML;
-			output.innerHTML = eval(output.innerHTML);
-		}
-		else {
-			if (output.innerHTML == "0") {
-				output.innerHTML = buttonValue;
-			}
-			else {
-				output.innerHTML += buttonValue;
-			}
-		}
-	});
+function updateDisplay() {
+  const display = document.getElementById("result");
+  display.innerText = currentDisplay;
 }
+
+function appendChar(char) {
+  if (currentDisplay === "0") {
+    currentDisplay = "";
+  }
+  currentDisplay += char;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentDisplay = "0";
+  updateDisplay();
+}
+
+function calculate() {
+  currentDisplay = eval(currentDisplay).toString
